598af2e5d7bccc2d3862882eaa73ee0294f35df4
silver <silveron.silveron@mail.ru>
2020-06-08 08:01:55 +0300

    Bubble sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,34 +1,17 @@
-import numpy
 
+def bubble_sort(array, reversed_order=False):
+    operation = "array[i] > array[j]"
 
-def bubble_sort(array):
-    for i in range(len(array) - 1):
-        for j in range(len(array) - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
+    if reversed_order:
+        operation = "array[i] < array[j]"
+
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if eval(operation):
+                array[i], array[j] = array[j], array[i]
 
     return array
+
+
+print(bubble_sort([-5, 6, 1, 4, 0], True))
 # O(n2) - bubble sort
-
-
-def insertion_sort(array):
-    for i in range(1, len(array)):
-        key = array[i]
-        j = i - 1
-
-        while j >= 0 and key < array[j]:
-            array[j + 1] = array[j]
-            j -= 1
-
-        array[j + 1] = key
-
-    return array
-
-# O(n2) - bad insertion sort
-# O(n) - best insertion sort
-
-
-x = numpy.random.randint(0, 100, 100)
-
-print(x)
-print(insertion_sort(x))
